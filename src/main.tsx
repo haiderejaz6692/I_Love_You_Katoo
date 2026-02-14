d6c34812ddff1382ab586e193bb1b98e9a47953f
haiderejaz6692 <m.haider.ijaz@gmail.com>
2026-02-14 06:18:15 +0100

    Add Cloudflare Web Analytics integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initCloudflareWebAnalytics } from "./lib/analytics";
+
+initCloudflareWebAnalytics();
 
 createRoot(document.getElementById("root")!).render(<App />);
